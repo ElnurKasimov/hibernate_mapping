a66cf3e6a76a416546842b2a93a187c19750c811
ElnurKasimov <ekasimov2012@gmail.com>
2023-08-03 17:19:17 +0300

    created ToDo

--- src/main/java/com/softserve/itacademy/model/ToDo.java
+++ src/main/java/com/softserve/itacademy/model/ToDo.java
@@ -2,15 +2,53 @@ package com.softserve.itacademy.model;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
 
+@Entity
+@Table(name = "todos")
 
 public class ToDo {
-//TODO
+    @Id
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(
+            name = "sequence-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @Parameter(name = "sequence_name", value = "role_sequence"),
+                    @Parameter(name = "initial_value", value = "100"),
+                    @Parameter(name = "increment_size", value = "1")
+            }
+    )
+    private long id;
+
+    private Instant createdAt;
+
+    @NotBlank (message = "title shouldn't be null, empty or blank")
+    private String title;
+
+    @NotNull (message = "owner shouldn't be null")
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    @ManyToMany
+    @JoinTable (
+            name = "todo_collaborator",
+            joinColumns = @JoinColumn(name = "todo_id"),
+            inverseJoinColumns = @JoinColumn(name = "collaborator_id")
+    )
+    private Set<User> collaborators;
+
+
+
 }
--- src/main/java/com/softserve/itacademy/model/User.java
+++ src/main/java/com/softserve/itacademy/model/User.java
@@ -1,6 +1,7 @@
 package com.softserve.itacademy.model;
 
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
@@ -8,7 +9,44 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import java.util.List;
+import java.util.Set;
 
+@Entity
+@Table(name = "users")
 public class User  {
-    //TODO
+    @Id
+    @GeneratedValue(generator = "sequence-generator")
+    @GenericGenerator(
+            name = "sequence-generator",
+            strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+            parameters = {
+                    @Parameter(name = "sequence_name", value = "role_sequence"),
+                    @Parameter(name = "initial_value", value = "100"),
+                    @Parameter(name = "increment_size", value = "1")
+            }
+    )
+    private long id;
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    @ManyToOne
+    @JoinColumn (name = "role_id")
+    private Role role;
+
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@_$#%&]).{8,}$", message =
+            "password should be 8 symbols or more and contain at least one letter, one capital letter,one number and one specific symbol")
+    private String password;
+
+
+    @OneToMany (mappedBy = "owner")
+    private Set<ToDo> ownerOf;
+
+    @ManyToMany (mappedBy = "collaborators")
+    private Set<ToDo> commonToDos;
+
+
 }
