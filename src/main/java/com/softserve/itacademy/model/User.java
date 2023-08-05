5b3a3b43d907e06bd78beae24e3397011cd3f556
ElnurKasimov <ekasimov2012@gmail.com>
2023-08-05 13:02:10 +0300

    User has been covered by tests for annotation

--- src/main/java/com/softserve/itacademy/model/User.java
+++ src/main/java/com/softserve/itacademy/model/User.java
@@ -30,13 +30,13 @@ public class User  {
     private String email;
 
     @NotBlank
-    @Size(min = 2, max = 255, message = "firstName has to be from 2 to 255 symbols")
-    @Pattern(regexp = "^[A-Za-z]+(?:-[A-Za-z]+)*$")
+    @Size(min = 2, max = 255, message = "firstName has to be from 2 to 255 symbols and can contain hyphen but not first, not last")
+    @Pattern(regexp = "^(?!-)[A-Z][a-zA-Z0-9]{1,251}(?:-[A-Z][a-zA-Z0-9]{2,})?(?<!-)$")
     private String firstName;
 
     @NotBlank
-    @Size(min = 2, max = 255, message = "lastName has to be from 2 to 255 symbols")
-    @Pattern(regexp = "^[A-Za-z]+(?:-[A-Za-z]+)*$")
+    @Size(min = 2, max = 255, message = "firstName has to be from 2 to 255 symbols and can contain hyphen but not first, not last")
+    @Pattern(regexp = "^(?!-)[A-Z][a-zA-Z0-9]{1,251}(?:-[A-Z][a-zA-Z0-9]{2,})?(?<!-)$")
     private String lastName;
 
     @NotNull
@@ -58,71 +58,72 @@ public class User  {
 
     public User() {
     }
-
+    @Generated
     public long getId() {
         return id;
     }
-
+    @Generated
     public void setId(long id) {
         this.id = id;
     }
-
+    @Generated
     public String getEmail() {
         return email;
     }
-
+    @Generated
     public void setEmail(String email) {
         this.email = email;
     }
-
+    @Generated
     public String getFirstName() {
         return firstName;
     }
-
+    @Generated
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
+    @Generated
     public String getLastName() {
         return lastName;
     }
-
+    @Generated
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
+    @Generated
     public Role getRole() {
         return role;
     }
-
+    @Generated
     public void setRole(Role role) {
         this.role = role;
     }
-
+    @Generated
     public String getPassword() {
         return password;
     }
-
+    @Generated
     public void setPassword(String password) {
         this.password = password;
     }
-
+    @Generated
     public Set<ToDo> getOwnerOf() {
         return ownerOf;
     }
-
+    @Generated
     public void setOwnerOf(Set<ToDo> ownerOf) {
         this.ownerOf = ownerOf;
     }
-
+    @Generated
     public Set<ToDo> getCommonToDos() {
         return commonToDos;
     }
-
+    @Generated
     public void setCommonToDos(Set<ToDo> commonToDos) {
         this.commonToDos = commonToDos;
     }
 
+    @Generated
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -130,12 +131,12 @@ public class User  {
         User user = (User) o;
         return getId() == user.getId() && Objects.equals(getEmail(), user.getEmail()) && Objects.equals(getFirstName(), user.getFirstName()) && Objects.equals(getLastName(), user.getLastName()) && Objects.equals(getRole(), user.getRole()) && Objects.equals(getPassword(), user.getPassword()) && Objects.equals(getOwnerOf(), user.getOwnerOf()) && Objects.equals(getCommonToDos(), user.getCommonToDos());
     }
-
+    @Generated
     @Override
     public int hashCode() {
         return Objects.hash(getId(), getEmail(), getFirstName(), getLastName(), getRole(), getPassword(), getOwnerOf(), getCommonToDos());
     }
-
+    @Generated
     @Override
     public String toString() {
         return "User{" +
